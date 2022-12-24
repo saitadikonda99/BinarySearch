75625ef94cfd7a9709b27042522d1db50b8513ad
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-24 10:47:26 +0530

    ceiling of number

--- Ceiling.java
+++ Ceiling.java
@@ -17,6 +17,7 @@ import java.util.Arrays;
 			arr[k] = in.nextInt();
 		
 	// input the target element 
+	// -1 if there is no ceiling element 
 		
 		System.out.print(" Enter the target element : ");
 		int target = in.nextInt();
@@ -25,11 +26,18 @@ import java.util.Arrays;
 	
 	// print the result
 
-	   System.out.println(" index : " + ans );	
+	   System.out.println( ans );	
 
 		}
 	
 	static int Ceiling( int[] arr , int target ) {
+
+// but what if the target element is greater than the greatest element in the array		
+		
+		if( target > arr[arr.length-1] ) {
+			 return -1;
+			}
+		
 		 int start = 0;
 		 int end   = arr.length - 1;
 		   	while( start <= end ) {
